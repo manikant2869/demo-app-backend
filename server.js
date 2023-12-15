9da96144aadda35c39e0a983254ced1bdf90ad0a
Manikant Kumar <manikant.kumar@ritzyware.com>
2023-12-15 12:09:18 +0530

    added message for v3 testing

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.get('/api/v1/test', (request, response, next) => {
 app.get('/api/v2/test', (request, response, next) => {
     response.send({ success: true }); 
 });
-
+app.get('/api/v3/test',(req,res)=>{
+    res.send({message: "v3 testing"});
+})
 server.listen(PORT, () => {
     console.log(`Server started at port: ${PORT}`);
 });
